aa682a8cae92be358abcc18693e4afb5477f3370
Hazim Sulaiman <hazim897@gmail.com>
2021-01-06 11:40:39 +0800

    add another consumer and docker-compose file for easy installation

--- consumer_b.py
+++ consumer_b.py
@@ -1,14 +1,16 @@
 from kafka import KafkaConsumer
+from kafka import TopicPartition
 import json
 
 if __name__ == '__main__':
     consumer = KafkaConsumer(
         'registered_user',
-        bootstrap_servers=['192.168.1.8:9092'],
+        bootstrap_servers=['192.168.1.2:9092'],
         auto_offset_reset='earliest',
-        group_id='consumer-group-a'
+        group_id='consumer-group-b'
     )
-    print("starting the consumer")
+
+    print("starting the consumer...")
 
     for msg in consumer:
         print("Registered User: {}".format(json.loads(msg.value)))
